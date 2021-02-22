e3233d4400d922b7adb8debf8d0c257c94a5704c
Frederik Majgaard Nielsen <10709@student.eadania.dk>
2021-02-22 15:59:46 +0100

    Add project files.

---
+++ øvelse 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace øvelse_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a list of numbers, separated by spaces:  ");
+            string line = Console.ReadLine();
+
+            List<string> list = line.Split(' ').ToList();
+            list.ForEach(Console.WriteLine);
+            List<int> numbers = list.ConvertAll(s => Int32.Parse(s));
+
+            Console.Write("The even numbers are ");
+            var result = numbers.Where(a => a % 2 == 0).ToList();
+            foreach (var item in result)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+
+            }
+            Console.WriteLine(".");
+
+            Console.ReadLine();
+        }
+    }
+}
